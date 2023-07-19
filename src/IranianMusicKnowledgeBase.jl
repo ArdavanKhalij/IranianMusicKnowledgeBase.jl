a2864f060551b9d75a24eef01f098715163e5625
Ardavan Khalij <44583966+ArdavanKhalij@users.noreply.github.com>
2023-07-19 09:32:14 +0330

    Update IranianMusicKnowledgeBase.jl

--- src/IranianMusicKnowledgeBase.jl
+++ src/IranianMusicKnowledgeBase.jl
@@ -366,7 +366,7 @@ function Load_Gushe_Notes(path::String,
 end
 
 function IR_Load_Data()
-    Data = JSON.parsefile("IRMusic_Structure_MirzaAbdollah.json")
+    Data = JSON.parsefile("IRMusic_Structure.json")
     R = Data["Radifs"]
     D = R[1]["Dastgahs"]
     A = []
